4a945ec07bbc80033e3395f1aac6b0b6e2c65ca3
elmoresiahaan <elmoresiahaan@gmail.com>
2022-09-09 20:34:57 -0600

    change

--- 03/Superhero/hero/urls.py
+++ 03/Superhero/hero/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import IndexView, SpidermanView, CaptainAmericaView, NarutoView
+from .views import IndexView, IchigoView, LuffyView, NarutoView
 
 urlpatterns = [
     path('', IndexView.as_view()),
-    path('spiderman', SpidermanView.as_view()),
-    path('capamerica', CaptainAmericaView.as_view()),
+    path('ichigo', IchigoView.as_view()),
+    path('luffy', LuffyView.as_view()),
     path('naruto', NarutoView.as_view()),
 ]
