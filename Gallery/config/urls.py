4d618cceca6452bb196cd007567647306a2c982c
elmoresiahaan <elmoresiahaan@gmail.com>
2022-09-14 21:57:09 -0600

    project 4

--- Gallery/config/urls.py
+++ Gallery/config/urls.py
@@ -1,7 +1,7 @@
 from django.views.generic import RedirectView
 from django.urls import path
 
-from photos.views import PhotoDetailView, PhotoListView
+from photos.views import PhotoDetailView, PhotoListView, LuffyView, NarutoView, SaitamaView, TanjiroView, NatsuView
 
 
 urlpatterns = [
@@ -12,4 +12,9 @@ urlpatterns = [
     # Photos
     path('photo/', PhotoListView.as_view()),
     path('photo/<int:id>', PhotoDetailView.as_view()),
+    path('photo/luffy', LuffyView.as_view()),
+    path('photo/naruto', NarutoView.as_view()),
+    path('photo/saitama', SaitamaView.as_view()),
+    path('photo/tanjiro', TanjiroView.as_view()),
+    path('photo/natsu', NatsuView.as_view()),
 ]
--- Gallery/photos/views.py
+++ Gallery/photos/views.py
@@ -31,11 +31,80 @@ class PhotoDetailView(TemplateView):
 
 
 class LuffyView(TemplateView):
-    template_name = "photo.html"
+    template_name = "phototest.html"
 
     def get_context_data(self, **kwargs):
         return {
             'title': 'Monkey D. Luffy',
-            'body': 'My name is Luffy and I will become the Pirate King.',
-            'image': '/static/images/luffy.jpg'
+            'image': '/static/images/33.jpg',
+            'strength1': 'conqueror\'s haki',
+            'strength2': 'immense physical strength',
+            'strength3': 'endurance and stamina',
+            'weakness1': 'impulsiveness',
+            'weakness2': 'prone to cutting or stabbing',
+            'weakness3': 'fire and ice',
+        }
+
+
+class NarutoView(TemplateView):
+    template_name = "phototest.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Naruto Uzumaki',
+            'image': '/static/images/11.jpg',
+            'strength1': 'all elements of chakra',
+            'strength2': 'sage mode',
+            'strength3': 'nine-tailed beast',
+            'weakness1': 'no skills in genjutsu',
+            'weakness2': 'too naive',
+            'weakness3': 'lack of ninjutsu diversity',
+        }
+
+
+class SaitamaView(TemplateView):
+    template_name = "phototest.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Saitama (Caped Baldy)',
+            'image': '/static/images/22.jpg',
+            'strength1': 'composure',
+            'strength2': 'shock-wave',
+            'strength3': 'speed and athleticism',
+            'weakness1': 'lack of technique and training in martial arts',
+            'weakness2': 'still human',
+            'weakness3': 'terrible at video games',
+        }
+
+
+class TanjiroView(TemplateView):
+    template_name = "phototest.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Tanjiro Kamado',
+            'image': '/static/images/44.jpg',
+            'strength1': 'immense stamina',
+            'strength2': 'endurance',
+            'strength3': 'control oxygen level',
+            'weakness1': 'too honest',
+            'weakness2': 'memory loss',
+            'weakness3': 'keeping sword intact',
+        }
+
+
+class NatsuView(TemplateView):
+    template_name = "phototest.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Natsu Dragneel',
+            'image': '/static/images/55.jpg',
+            'strength1': 'ability to consume flames',
+            'strength2': 'immune to explosions',
+            'strength3': 'fire dragon slayer magic',
+            'weakness1': 'motion sickness',
+            'weakness2': 'not planning ahead',
+            'weakness3': 'impulsive and hasty',
         }
