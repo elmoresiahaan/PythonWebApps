d6d7a8b63dbe9c5b3bcf4bb90221373bbc318816
elmoresiahaan <elmoresiahaan@gmail.com>
2022-11-07 18:41:29 -0700

    image

--- Superhero/hero/models.py
+++ Superhero/hero/models.py
@@ -8,8 +8,8 @@ class Superhero(models.Model):
     name = models.CharField(max_length=200)
     identity = models.CharField(max_length=200)
     description = models.TextField(default="None")
-    # image = models.CharField(max_length=200, default="None")
-    image = models.ForeignKey(Photo, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.CharField(max_length=200, default="None")
+    #image = models.ForeignKey(Photo, on_delete=models.CASCADE, null=True, blank=True)
     strengths = models.CharField(max_length=200, default="None")
     weaknesses = models.CharField(max_length=200, default="None")
 
